2a71936eed5668d5c72c441c128defb2ee2bae2b
Collabsvito <armishath88@gmail.com>
2019-07-14 19:52:36 +0200

    Update of SPIEGEL.TV to v.1.0.1
    - fixed: Rubrik = Sendungen Fehler beim öffnen des Ordners
    - fixed: Rubrik = Studios es wird KEIN Inhalt angezeigt
    - added: Rubrik = Popcorn, WEB-TV Playliste

--- plugin.video.L0RE.spiegeltv/default.py
+++ plugin.video.L0RE.spiegeltv/default.py
@@ -1,239 +1,223 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sys
-import urlparse
+import os
+import re
+import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
-import xbmc
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+if PY2:
+	from urllib import quote, unquote, quote_plus, unquote_plus, urlencode  # Python 2.X
+	from urllib2 import build_opener, HTTPCookieProcessor, Request, urlopen  # Python 2.X
+	from cookielib import LWPCookieJar  # Python 2.X
+	from urlparse import urljoin, urlparse, urlunparse  # Python 2.X
+elif PY3:
+	from urllib.parse import quote, unquote, quote_plus, unquote_plus, urlencode, urljoin, urlparse, urlunparse  # Python 3+
+	from urllib.request import build_opener, HTTPCookieProcessor, Request, urlopen  # Python 3+
+	from http.cookiejar import LWPCookieJar  # Python 3+
+import json
 import xbmcvfs
-import urllib, urllib2, socket, cookielib, re, os, shutil,json
+import shutil
+import socket
 import time
-import base64
-import requests
+import io
+import gzip
 from bs4 import BeautifulSoup
-from HTMLParser import HTMLParser
 import YDStreamExtractor
 
-# Setting Variablen Des Plugins
+
 global debuging
-base_url = sys.argv[0]
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+pluginhandle = int(sys.argv[1])
 addon = xbmcaddon.Addon()
-# Lade Sprach Variablen
-translation = addon.getLocalizedString
+addonPath = xbmc.translatePath(addon.getAddonInfo('path')).encode('utf-8').decode('utf-8')
+dataPath    = xbmc.translatePath(addon.getAddonInfo('profile')).encode('utf-8').decode('utf-8')
+temp           = xbmc.translatePath(os.path.join(dataPath, 'temp', '')).encode('utf-8').decode('utf-8')
+defaultFanart = os.path.join(addonPath, 'fanart.jpg')
+icon = os.path.join(addonPath, 'icon.png')
+baseURL = "https://www.spiegel.tv"
 
 
-xbmcplugin.setContent(addon_handle, 'Movies')
-
-xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_UNSORTED)
-xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_VIDEO_SORT_TITLE)
-
-
-icon = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')+'/icon.png').decode('utf-8')
-
-
-profile    = xbmc.translatePath( addon.getAddonInfo('profile') ).decode("utf-8")
-temp       = xbmc.translatePath( os.path.join( profile, 'temp', '') ).decode("utf-8")
-
-if xbmcvfs.exists(temp):
-  shutil.rmtree(temp)
+if xbmcvfs.exists(temp) and os.path.isdir(temp):
+	shutil.rmtree(temp, ignore_errors=True)
+	xbmc.sleep(500)
 xbmcvfs.mkdirs(temp)
-cookie=os.path.join( temp, 'cookie.jar')
-cj = cookielib.LWPCookieJar();
+cookie = os.path.join(temp, 'cookie.lwp')
+cj = LWPCookieJar()
 
 if xbmcvfs.exists(cookie):
-    cj.load(cookie,ignore_discard=True, ignore_expires=True)                  
+	cj.load(cookie, ignore_discard=True, ignore_expires=True)
+
+def py2_enc(s, encoding='utf-8'):
+	if PY2 and isinstance(s, unicode):
+		s = s.encode(encoding)
+	return s
+
+def py2_uni(s, encoding='utf-8'):
+	if PY2 and isinstance(s, str):
+		s = unicode(s, encoding)
+	return s
+
+def py3_dec(d, encoding='utf-8'):
+	if PY3 and isinstance(d, bytes):
+		d = d.decode(encoding)
+	return d
+
+def translation(id):
+	LANGUAGE = addon.getLocalizedString(id)
+	LANGUAGE = py2_enc(LANGUAGE)
+	return LANGUAGE
+
+def failing(content):
+	log(content, xbmc.LOGERROR)
 
 def debug(content):
-    log(content, xbmc.LOGDEBUG)
-    
-def notice(content):
-    log(content, xbmc.LOGNOTICE)
+	log(content, xbmc.LOGDEBUG)
 
 def log(msg, level=xbmc.LOGNOTICE):
-    addon = xbmcaddon.Addon()
-    addonID = addon.getAddonInfo('id')
-    xbmc.log('%s: %s' % (addonID, msg), level) 
-    
+	msg = py2_enc(msg)
+	xbmc.log("["+addon.getAddonInfo('id')+"-"+addon.getAddonInfo('version')+"]"+msg, level)
 
+def getUrl(url, header=None):
+	global cj
+	opener = build_opener(HTTPCookieProcessor(cj))
+	try:
+		if header:
+			opener.addheaders = header
+		else:
+			opener.addheaders =[('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:60.0) Gecko/20100101 Firefox/60.0')]
+			opener.addheaders = [('Accept-Encoding', 'gzip, deflate')]
+		response = opener.open(url, timeout=30)
+		if response.info().get('Content-Encoding') == 'gzip':
+			content = py3_dec(gzip.GzipFile(fileobj=io.BytesIO(response.read())).read())
+		else:
+			content = py3_dec(response.read())
+	except Exception as e:
+		failure = str(e)
+		if hasattr(e, 'code'):
+			failing("(getUrl) ERROR - ERROR - ERROR : ########## {0} === {1} ##########".format(url, failure))
+		elif hasattr(e, 'reason'):
+			failing("(getUrl) ERROR - ERROR - ERROR : ########## {0} === {1} ##########".format(url, failure))
+		content = ""
+		return sys.exit(0)
+	opener.close()
+	try: cj.save(cookie, ignore_discard=True, ignore_expires=True)
+	except: pass
+	return content
+
+def index():
+	addDir('Popcorn - Zurücklehnen und Film schauen', baseURL+'/playlists/2543-popcorn-film-schauen-und-zuruecklehnen', 'listVideos', icon, sub='special')
+	addDir('Sendungen', '0', 'listThemes', icon)
+	addDir('Stöbern', '2', 'listThemes', icon)
+	addDir('Studios', baseURL+'/allstudios', 'listStudios', icon)
+	addDir('Meist gesehen', baseURL+'/hot', 'listVideos', icon, sub='normal')
+	addDir('Neue Beiträge', baseURL+'/new', 'listVideos', icon, sub='normal')
+	addDir('WEB-TV Playliste', baseURL+'/playlists/1395-webtv', 'listVideos', icon, sub='special')
+	xbmcplugin.endOfDirectory(pluginhandle)
+
+def listThemes(url):
+	xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_LABEL)
+	content = getUrl(baseURL+'/start')
+	selection = re.findall("data-target='categoriesholder{0}'><div id='catsholder{0}'>(.+?)</ul></div></div>".format(str(url)), content, re.DOTALL)
+	for chtml in selection:
+		part = chtml.split("class='underlinemagic'>")
+		for i in range(1, len(part), 1):
+			element = part[i]
+			link = baseURL+re.compile("<a href='([^']+?)' data", re.DOTALL).findall(element)[0].strip()
+			title = re.compile("data-navigateparam=.+?'>([^<]+?)</a>", re.DOTALL).findall(element)[0]
+			title = py2_enc(title).strip()
+			addDir(title, link, 'listVideos', icon, sub='special')
+	xbmcplugin.endOfDirectory(pluginhandle)
   
-def addDir(name, url, mode, thump, desc="",page=1,sub="",serie=""):   
-  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&page="+str(page)+"&sub="+str(sub)+"&serie="+str(serie)
-  ok = True
-  liz = xbmcgui.ListItem(name)  
-  liz.setArt({ 'fanart' : thump })
-  liz.setArt({ 'thumb' : thump })
-  liz.setArt({ 'banner' : icon })
-  liz.setArt({ 'fanart' : icon })
-  liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc})
-	
-  ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
-  return ok
+def listStudios(url):
+	xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_LABEL)
+	content = getUrl(url)
+	htmlPage = BeautifulSoup(content, 'html.parser')  
+	stud_block = htmlPage.find('div',attrs={'id':'rcblock'}) 
+	Movies = stud_block.find_all('a',attrs={'data-navigateto':'withstudio'}) 
+	for video in Movies:
+		link = baseURL+video['href']
+		image = video.find('img')['src']
+		title = video.find('img')['alt']
+		addDir(title, link, 'listVideos', image, sub='normal', serie=title)
+	xbmcplugin.endOfDirectory(pluginhandle)  
   
-def addLink(name, url, mode, thump, duration="", desc="", genre='',director="",bewertung="",serie=""):
-  debug("URL ADDLINK :"+url)
-  debug( icon  )
-  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
-  ok = True
-  liz = xbmcgui.ListItem(name,thumbnailImage=thump)
-  liz.setArt({ 'fanart' : icon })
-  liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc, "Genre": genre, "Director":director,"Rating":bewertung,"TVShowTitle":serie})
-  liz.setProperty('IsPlayable', 'true')
-  liz.addStreamInfo('video', { 'duration' : duration })
-	#xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
-  ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
-  return ok
-  
+def listVideos(url, sub, serie):
+	content = getUrl(url)
+	htmlPage = BeautifulSoup(content, 'html.parser')
+	vid_block = htmlPage.find('div',attrs={'id':'rcblock'})
+	if sub == 'special':
+		Movies = vid_block.find_all('a',attrs={'class':'focusable tholder material'}) 
+	else:
+		Movies = vid_block.find_all('a',attrs={'class':'focusable vholder material fortwolines'})
+	for video in Movies:
+		link = baseURL+video['href']
+		image = video.find('img')['src']
+		tagline = video.find('div',attrs={'class':'autocut bold cardsubtitle'}).get_text()
+		plot = "[B][COLOR FF76EE00]"+tagline+"[/COLOR][/B][CR]"
+		if sub == 'special':
+			title = video.find('div',attrs={'class':'autocut bold cardtitle'}).get_text()
+			plot += video.find('div',attrs={'class':'tdesc'}).get_text()
+			matchD = video.find('div',attrs={'class':'tholderbottom'}).get_text()
+		else:
+			title = video.find('div',attrs={'class':'twolines bold cardtitle'}).get_text()
+			matchD = video.find('div',attrs={'class':'autocut addinfo'}).get_text()
+		duration = re.compile('([0-9]+)', re.DOTALL).findall(matchD)[0]    
+		duration = int(duration)*60
+		addLink(title, link, 'playVideo', image, duration, plot, tagline, serie)
+	xbmcplugin.endOfDirectory(pluginhandle)  
+
+def playVideo(url):    
+	vid = YDStreamExtractor.getVideoInfo(url, quality=2) # quality is 0=SD, 1=720p, 2=1080p and is a maximum
+	stream_url = vid.streamURL() # This is what Kodi (XBMC) will play
+	stream_url = stream_url.split('|')[0]
+	xbmcplugin.setResolvedUrl(pluginhandle, True, xbmcgui.ListItem(path=stream_url))
+
 def parameters_string_to_dict(parameters):
 	paramDict = {}
 	if parameters:
-		paramPairs = parameters[1:].split("&")
+		paramPairs = parameters[1:].split('&')
 		for paramsPair in paramPairs:
 			paramSplits = paramsPair.split('=')
 			if (len(paramSplits)) == 2:
 				paramDict[paramSplits[0]] = paramSplits[1]
 	return paramDict
 
+def addDir(name, url, mode, iconimage, plot=None, sub=None, serie=None):   
+	u = sys.argv[0]+"?url="+quote_plus(url)+"&mode="+str(mode)+"&sub="+str(sub)+"&serie="+str(serie)
+	liz = xbmcgui.ListItem(name, iconImage=icon, thumbnailImage=iconimage)
+	liz.setInfo(type='Video', infoLabels={'TvShowtitle': serie, 'Title': name, 'Plot': plot})
+	liz.setArt({'poster': iconimage, 'fanart': defaultFanart})
+	xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+	return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
 
-def geturl(url,data="x",header="",referer=""):
-        global cj
-        debug("Get Url: " +url)
-        for cook in cj:
-          debug(" Cookie :"+ str(cook))
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))        
-        userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
-        if header=="":
-          opener.addheaders = [('User-Agent', userAgent)]        
-        else:
-          opener.addheaders = header        
-        if not referer=="":
-           opener.addheaders = [('Referer', referer)]
+def addLink(name, url, mode, iconimage, duration=None, plot=None, tagline=None, serie=None):
+	u = sys.argv[0]+"?url="+quote_plus(url)+"&mode="+str(mode)
+	liz = xbmcgui.ListItem(name, iconImage=icon, thumbnailImage=iconimage)
+	liz.setInfo(type='Video', infoLabels={'TvShowtitle': serie, 'Title': name, 'Plot': plot, 'Duration': duration, 'Tagline': tagline, 'Studio': 'Spiegel.tv', 'mediatype': 'video'})
+	liz.setArt({'poster': iconimage, 'fanart': defaultFanart, 'landscape': iconimage})
+	liz.addStreamInfo('Video', {'Duration': duration})
+	liz.setProperty('IsPlayable', 'true')
+	liz.setContentLookup(False)
+	xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
+	return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
 
-        try:
-          if data!="x" :
-             content=opener.open(url,data=data).read()
-          else:
-             content=opener.open(url).read()
-        except urllib2.HTTPError as e:
-             #debug( e.code )  
-             cc=e.read()  
-             debug("Error : " +cc)
-             content=""
-       
-        opener.close()
-        cj.save(cookie,ignore_discard=True, ignore_expires=True)               
-        return content
-
-
-  
-def liste():   
-    addDir("Sendungen" , "categories1", "themenliste","")   
-    addDir("Stöbern" , "categories2", "themenliste","")   
-    addDir("Studios" , "http://www.spiegel.tv/allstudios", "startrubrik","",sub="studios")   
-    addDir("Meist gesehen" , "http://www.spiegel.tv/hot", "startrubrik","",sub="videos")      
-    addDir("Neues" , "http://www.spiegel.tv/new  ", "startrubrik","",sub="videos")       
-    xbmcplugin.endOfDirectory(addon_handle) 
-
-def playvideo(url):    
-   debug(url)
-   vid = YDStreamExtractor.getVideoInfo(url,quality=2) #quality is 0=SD, 1=720p, 2=1080p and is a maximum
-   debug(vid)
-   stream_url = vid.streamURL()
-   stream_url=stream_url.split("|")[0]
-   listitem = xbmcgui.ListItem(path=stream_url)   
-   addon_handle = int(sys.argv[1])  
-   xbmcplugin.setResolvedUrl(addon_handle, True, listitem)
-
-
-    
 params = parameters_string_to_dict(sys.argv[2])
-mode = urllib.unquote_plus(params.get('mode', ''))
-url = urllib.unquote_plus(params.get('url', ''))
-referer = urllib.unquote_plus(params.get('referer', ''))
-page= urllib.unquote_plus(params.get('page', ''))
-sub= urllib.unquote_plus(params.get('sub', ''))
-serie= urllib.unquote_plus(params.get('serie', ''))
+url = unquote_plus(params.get('url', ''))
+mode = unquote_plus(params.get('mode', ''))
+sub= unquote_plus(params.get('sub', ''))
+serie= unquote_plus(params.get('serie', ''))
 
-def themenliste(url):
-  starturl="http://www.spiegel.tv/start"  
-  content=geturl(starturl)
-  htmlPage = BeautifulSoup(content, 'html.parser')  
-  themen_subs = htmlPage.find("ul",attrs={"id":url})  
-  themen = themen_subs.find_all("li",attrs={"class":"underlinemagic"})    
-  for thema in themen:          
-    link_html=thema.find("a")
-    link="http://www.spiegel.tv"+link_html["href"]
-    text=link_html.text       
-    addDir(text,link,"videoliste","")  
-  xbmcplugin.endOfDirectory(addon_handle)
-  
-def startrubrik(url,sub):
-  content=geturl(url)
-  htmlPage = BeautifulSoup(content, 'html.parser')  
-  Videos_block = htmlPage.find("div",attrs={"id":"rcblock"}) 
-  Videos = Videos_block.find_all("a",attrs={"data-navigateto":sub}) 
-  for video in Videos:
-    link="http://www.spiegel.tv"+video["href"]
-    image = video.find("img")["src"]
-    title = video.find("img")["alt"]
-    if sub=="studios":
-      addDir(title,link,"videoliste",image,serie=title)
-    else:
-     addLink(title,link,"playvideo",image)
-  xbmcplugin.endOfDirectory(addon_handle)  
-  
-def videoliste(url,serie):
-  xbmcplugin.setContent(addon_handle, 'episodes')
-  content=geturl(url)
-  htmlPage = BeautifulSoup(content, 'html.parser')  
-  Videos_block = htmlPage.find("div",attrs={"id":"rcblock"}) 
-  Videos = Videos_block.find_all("a",attrs={"class":"focusable tholder material"}) 
-  for video in Videos:
-    debug("---")
-    debug(video)
-    link="http://www.spiegel.tv"+video["href"]
-    image = video.find("img")["src"]
-    title = video.find("div",attrs={"class":"autocut bold cardtitle"}).text
-    desc = video.find("div",attrs={"class":"tdesc"}).text    
-    durationstring = video.find("div",attrs={"class":"tholderbottom"}).text
-    duration=re.compile('([0-9]+)', re.DOTALL).findall(durationstring)[0]    
-    duration=int(duration)*60
-    addLink(title,link,"playvideo",image,desc=desc,serie=serie,duration=duration)
-  xbmcplugin.endOfDirectory(addon_handle)  
-  
-  
-def playlive():
- url="http://www.weltderwunder.de/videos/live"
- content=geturl(url)
- htmlPage = BeautifulSoup(content, 'html.parser')
- videoinfo = htmlPage.find("iframe")["src"]
- content=geturl(videoinfo)
- debug(content)
- videoliste=re.compile('"applehttp","url":"(.+?)"', re.DOTALL).findall(content)[0]
- videoliste=videoliste.replace("\\/","/")
- listitem = xbmcgui.ListItem(path=videoliste)   
- addon_handle = int(sys.argv[1])  
- xbmcplugin.setResolvedUrl(addon_handle, True, listitem)
- 
- 
-# Haupt Menu Anzeigen      
-if mode is '':
-     liste()   
+if mode == 'listThemes':
+	listThemes(url)  
+elif mode == 'listStudios':
+	listStudios(url)
+elif mode == 'listVideos':
+	listVideos(url, sub, serie)
+elif mode == 'playVideo':
+	playVideo(url)
 else:
-  # Wenn Settings ausgewählt wurde
-  if mode == 'Settings':
-          addon.openSettings()
-  # Wenn Kategory ausgewählt wurde
-  if mode == 'playvideo':
-          playvideo(url)
-  if mode == 'themenliste':
-          themenliste(url)  
-  if mode == 'playlive':
-          playlive()
-  if mode == 'videoliste':
-          videoliste(url,serie)  
-  if mode == 'startrubrik':
-          startrubrik(url,sub)  
+	index()
